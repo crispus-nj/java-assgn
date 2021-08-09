6fd99191382a9ef25e4d62489a5c7aeeba695f8f
kingi001 <kingikhamis518@gmail.com>
2021-08-09 13:43:00 +0300

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Student S1 = new Student("khamis kingi", "male",24);
+        GradStudent G1 = new GradStudent("Oyiersupervisor","sct121-07072018");
+        System.out.println("name"+S1.getName(),"gender"+S1.getGender(),"age"+S1.getAge());
+        System.out.println("supervisorname"+G1.getSupName(),"reistration number"+G1.getRegNo());
 
     }
 }
